0a33053b739c7c5d1172c681e839be755defba9e
Jose Ruiz <jruiz793ric@gmail.com>
2024-08-09 19:29:10 -0300

    Creando las rutas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("hello world");
+import express from 'express';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express()
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+app.set('cliente', join(__dirname, 'cliente'))
+
+app.get(`/`, (req, res) => {
+    res.sendFile(process.cwd() + '/src/cliente/index.html')
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(process.cwd() + '/src/cliente/about.html')
+})
+
+app.get('/contact', (req, res) => {
+    res.sendFile(process.cwd() + '/src/cliente/contact.html')
+})
+
+app.listen(3001)
+console.log('Hola express')
